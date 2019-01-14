d26a3f3aed49f81cf020256ffa7a3b3741deac16
Will <will.mcdaniell@tamu.edu>
2019-01-13 21:29:54 -0600

    start adding basics

--- src/App.js
+++ src/App.js
@@ -1,28 +1,89 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Input from "@material-ui/core/Input";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import NoteAddIcon from "@material-ui/icons/NoteAdd";
+
+const styles = theme => ({
+    paper: {
+        margin: theme.spacing.unit * 2,
+        padding: theme.spacing.unit * 2,
+        width: "50vw",
+        maxWidth: "700px"
+    },
+    input: {
+        width: "50%"
+    },
+    addButton: {
+        padding: theme.spacing.unit,
+        margin: theme.spacing.unit
+    },
+    buttonText: {
+        margin: `0 ${theme.spacing.unit}px`
+    }
+});
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        curVal: "",
+        todos: []
+    };
+
+    onChange = e => this.setState({ curVal: e.target.value });
+
+    onAddTodo = () =>
+        this.setState(({ curVal, todos }) => {
+            return {
+                curVal: "",
+                todos: [...todos, curVal]
+            };
+        });
+
+    render() {
+        const { classes } = this.props;
+        const { curVal } = this.state;
+        return (
+            <Grid
+                container
+                direction="column"
+                alignContent="center"
+                alignItems="center"
+                justify="center"
+            >
+                <Grid item>
+                    <Paper className={classes.paper}>
+                        <Input
+                            type="text"
+                            placeholder="...new todo"
+                            multiline={true}
+                            rows={1}
+                            rowsMax={5}
+                            onChange={this.onChange}
+                            value={curVal}
+                            className={classes.input}
+                        />
+                        <Button
+                            variant="contained"
+                            disabled={!curVal}
+                            onClick={this.onAddTodo}
+                            className={classes.addButton}
+                        >
+                            <Typography
+                                className={classes.buttonText}
+                                color="inherit"
+                            >
+                                Add
+                            </Typography>
+                            <NoteAddIcon />
+                        </Button>
+                    </Paper>
+                </Grid>
+            </Grid>
+        );
+    }
 }
 
-export default App;
+export default withStyles(styles)(App);
