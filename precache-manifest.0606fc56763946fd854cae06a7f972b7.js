1ae749623bc3aa22b0b9730558e158adeecc39bf
Will <will.mcdaniell@tamu.edu>
2019-01-27 08:51:16 -0600

    Updates

---
+++ precache-manifest.0606fc56763946fd854cae06a7f972b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "1fccc33a7043835930d1",
+    "url": "/todo/static/js/main.1fccc33a.chunk.js"
+  },
+  {
+    "revision": "d759dca37c8172b2711c",
+    "url": "/todo/static/js/1.d759dca3.chunk.js"
+  },
+  {
+    "revision": "1fccc33a7043835930d1",
+    "url": "/todo/static/css/main.d9d018ff.chunk.css"
+  },
+  {
+    "revision": "d27bdc43474a5a5de28b4317a55505af",
+    "url": "/todo/index.html"
+  }
+];
